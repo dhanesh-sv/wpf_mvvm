3b8ada69a9031261f276564079bfc5980ce37cd5
DhaneshS <dhanesh.sv@gmail.com>
2017-11-08 11:27:43 +0530

    ddd

--- wpf_mvvm_sample/ViewModels/ViewModelLocator.cs
+++ wpf_mvvm_sample/ViewModels/ViewModelLocator.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace wpf_mvvm_sample.ViewModels
 {
     class ViewModelLocator
     {
+       // MessageBox.Show("sds");
     }
 }
